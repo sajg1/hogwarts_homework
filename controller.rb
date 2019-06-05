b5e236e296973139ac0066ceb5f8e94df96b6ba1
Steve Gossip <stephengossip@gmail.com>
2019-06-05 18:50:46 +0100

    Part 1 complete

--- controller.rb
+++ controller.rb
@@ -6,10 +6,26 @@ require_relative('./models/student')
 
 also_reload('./models/*')
 
+# INDEX
+
 get '/students' do
   @students = Student.all()
   erb(:index)
 end
+
+# CREATE
+
+post '/students' do
+  @student = Student.new(params)
+  @student.save()
+  erb(:create)
+end
+# NEW
+get '/students/new' do
+  erb(:new)
+end
+
+# SHOW
 
 get '/students/:id' do
   @student = Student.find_by_id(params[:id].to_i)
